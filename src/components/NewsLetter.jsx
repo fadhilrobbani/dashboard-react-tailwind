9e2c36792f598204317c11a8ee9697f270511850
fadhilrobbani <rabbanifadhillailham@gmail.com>
2022-08-26 20:09:22 +0700

    add input state

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const NewsLetter = () => {
+  const [input, setInput] = useState('');
+  const [invalid, setInvalid] = useState('text-slate-900');
+  const onChangeInput = useCallback((ev) => {
+    setInput(ev.currentTarget.value);
+    ev.currentTarget.value.includes('@')
+      ? setInvalid('text-slate-900')
+      : setInvalid('text-pink-600 focus:ring-pink-600');
+  });
+
   return (
     <div className='bg-slate-900 mx-auto py-10 px-6 grid md:grid-cols-2 md:items-center gap-2'>
       <div>
@@ -11,16 +20,23 @@ const NewsLetter = () => {
         <p>Sign up to our newslatter and stay to up to date</p>
       </div>
       <div>
-        <div className='mt-8 flex flex-col items-center sm:flex-row gap-4 justify-center '>
-          <input
-            type='text'
-            className='w-full rounded-md text-slate-900 py-2 px-4'
-            placeholder='Enter your email'
-          />
-          <button className='w-full sm:w-36 bg-green-600 min-w-[120px] rounded-md py-2 px-4'>
-            Notify Me
-          </button>
-        </div>
+        <form action=''>
+          <div className='mt-8 flex flex-col items-center sm:flex-row gap-4 justify-center '>
+            <input
+              type='text'
+              onChange={onChangeInput}
+              value={input}
+              className={`focus:outline-none focus:ring-green-700 focus:ring-4 w-full rounded-md py-2 px-4 ${invalid}`}
+              placeholder='Enter your email'
+            />
+            <button
+              type='submit'
+              className='w-full sm:w-36 bg-green-600 min-w-[120px] rounded-md py-2 px-4'
+            >
+              Notify Me
+            </button>
+          </div>
+        </form>
         <div className='mt-4'>
           <p className='text-sm'>
             We care about protection for your privacy and data. Read Our
