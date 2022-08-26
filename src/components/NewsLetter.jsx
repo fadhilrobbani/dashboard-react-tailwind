72c2c65e7c78b63602ddd64ece210a47c61bed03
fadhilrobbani <rabbanifadhillailham@gmail.com>
2022-08-26 20:45:57 +0700

    fix bug

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const NewsLetter = () => {
@@ -6,13 +6,13 @@ const NewsLetter = () => {
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const [input, setInput] = useState('');
   const [invalid, setInvalid] = useState('text-slate-900');
-  const onChangeInput = useCallback((ev) => {
+  const onSubmitHandler = (ev) => ev.preventDefault();
+  const onChangeInput = (ev) => {
     setInput(ev.currentTarget.value);
     emailRegex.test(ev.currentTarget.value) || ev.currentTarget.value === ''
       ? setInvalid('text-slate-900')
       : setInvalid('text-pink-600 focus:ring-pink-600');
-  });
-
+  };
   return (
     <div className='bg-slate-900 mx-auto py-10 px-6 grid md:grid-cols-2 md:items-center gap-2'>
       <div>
@@ -22,7 +22,7 @@ const NewsLetter = () => {
         <p>Sign up to our newslatter and stay to up to date</p>
       </div>
       <div>
-        <form action=''>
+        <form onSubmit={onSubmitHandler}>
           <div className='mt-8 flex flex-col items-center sm:flex-row gap-4 justify-center '>
             <input
               type='text'
@@ -34,7 +34,7 @@ const NewsLetter = () => {
             />
             <button
               type='submit'
-              className='w-full sm:w-36 bg-green-600 min-w-[120px] rounded-md py-2 px-4 hover:bg-transparent hover:ring-2 hover:ring-green-600'
+              className='w-full sm:w-36 bg-green-700 min-w-[120px] rounded-md py-2 px-4 hover:bg-transparent hover:ring-2 hover:ring-green-600'
             >
               Notify Me
             </button>
